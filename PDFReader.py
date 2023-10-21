9f8929beb0db27e7b0b785f3b322b1477799b184
Noel Sengel <123399929+NoelSengel@users.noreply.github.com>
2023-10-21 15:07:33 -0500

    Update PDFReader.py

--- PDFReader.py
+++ PDFReader.py
@@ -1,15 +1,21 @@
-import pyttsx3,PyPDF2
+import pyttsx3, PyPDF2
 
-#insert name of your pdf 
-pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
+title = input("Which book would you like to listen to?\n") + ".pdf"
+pdfreader = PyPDF2.PdfReader(open(title, 'rb'))
 speaker = pyttsx3.init()
 
-for page_num in range(pdfreader.numPages):
-    text = pdfreader.getPage(page_num).extractText()
-    clean_text = text.strip().replace('\n', ' ')
-    print(clean_text)
-#name mp3 file whatever you would like
-speaker.save_to_file(clean_text, 'story.mp3')
-speaker.runAndWait()
+first_page = input("Starting with what page?\n")
+last_page = input("Ending with what page?\n")
+
+text = ""
+
+for page_number in range(int(first_page - 1), int(last_page)):
+      text = text + pdfreader.pages[page_number].extract_text()
+
+clean_text = text.strip().replace('\n', ' ')
+print(clean_text)
+
+speaker.save_to_file(clean_text, 'result2.mp3')
+speaker.runAndWait
 
 speaker.stop()
